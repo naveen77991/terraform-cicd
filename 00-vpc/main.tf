bb18ee95b689b07d8d8b6b019885a31d25c5cee5
naveen77991 <162993940+naveen77991@users.noreply.github.com>
2026-04-17 11:42:40 +0530

    Update main.tf

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,1 +1,74 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.project}-${var.env}-vpc"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Public Subnet
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+}
+
+# Private Subnet
+resource "aws_subnet" "private" {
+  count = 2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+}
+
+# AZ Data
+data "aws_availability_zones" "available" {}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route to Internet
+resource "aws_route" "internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Associate Public Subnets
+resource "aws_route_table_association" "public_assoc" {
+  count = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# VPC Peering with Default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_vpc_peering_connection" "peer" {
+  vpc_id        = aws_vpc.main.id
+  peer_vpc_id   = data.aws_vpc.default.id
+  auto_accept   = true
+}
+
+# Route to Default VPC (IMPORTANT FIX)
+resource "aws_route" "peer_route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}

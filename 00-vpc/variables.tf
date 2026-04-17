0b8fadc4f18085e792ed11d195eaf71feeb3c953
naveen77991 <162993940+naveen77991@users.noreply.github.com>
2026-04-17 12:31:15 +0530

    Update variables.tf

--- 00-vpc/variables.tf
+++ 00-vpc/variables.tf
@@ -1,19 +1,3 @@
-variable "project" {
-  default = "roboshop-dev"
-}
-
-variable "env" {
-  default = "dev"
-}
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "public_subnets" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnets" {
-  default = ["10.0.11.0/24", "10.0.12.0/24"]
+variable "region" {
+  default = "us-east-1"
 }
